5a8d2486a16a3474e8b5267bca09848cba3cf0a4
jschmidmb <jschmid@meadowbrook-ma.org>
2023-10-23 10:32:39 -0400

    update to add session filter

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const ageFilter = document.getElementById('grade-filter');
     const strandFilter = document.getElementById('strand-filter');
     const courseList = document.querySelectorAll('.course');
+    const sessionFilter = document.querySelectorAll('.session');
 
     ageFilter.addEventListener('change', filterCourses);
     strandFilter.addEventListener('change', filterCourses);
+    sessionFilter.addEventListener('change', filterCourses);
 
     function filterCourses() {
         const selectedAge = ageFilter.value;
         const selectedStrand = strandFilter.value;
+        const selectedSession = sessionFilter.value;
 
         courseList.forEach(course => {
             const courseAge = course.getAttribute('data-grade');
             const courseStrand = course.getAttribute('data-strand');
+            const courseSession = course.getAttribute('data-session');
 
             if ((selectedAge === 'all' || selectedAge === courseAge) &&
-                (selectedStrand === 'all' || selectedStrand === courseStrand)) {
+                (selectedStrand === 'all' || selectedStrand === courseStrand)) &&
+                (selectedSession === 'all' || selectedSession === courseSession)) {
                 course.style.display = 'block';
             } else {
                 course.style.display = 'none';
